4059308e1a0efd4ebc80a64cc039609ea9830777
Alperen Tunc <115324004+alperen-tunc@users.noreply.github.com>
2023-04-05 14:18:46 +0200

    update_05_04

--- javaScript.js
+++ javaScript.js
@@ -161,6 +161,131 @@ function event1(element)
 }
 
 
+function event2()
+{
+    var bBody = document.getElementById("Paragraph4")
+    window.alert(bBody.parentNode);
+}
+
+function event3()
+{
+    var event3 = document.getElementById("div2");
+    var paragpaph = document.createElement("p");
+    paragpaph.innerHTML = "JavaScript ist echt easy";
+    event3.appendChild(paragpaph);
+}
+
+function event4()
+{
+    //window.alert(window.location.protocol);
+    window.open("https://www.google.com", '_blank');
+}
+
+function event5()
+{
+    window.history.forward();
+}
+
+function event6()
+{
+    var input = window.prompt("Geben Sie hier was ein", "default");
+    if(input == "google")
+    {
+        window.open("https://www.google.com", '_blank');
+    }
+}
+
+function event7()
+{
+    document.cookie = "isread=1; 10 Apr 2023 12:00:00 UTC" ;
+    var forCookies = document.cookie;
+    window.alert(forCookies);
+}
+
+function Objekt()
+{
+    var pinguen =
+    {
+        name: "Franz",
+        alter: 40,
+        schwimmgeschwindigkeit: 4
+    };
+
+    window.alert(pinguen.name);
+}
+
+function Objekt2()
+{
+    class Pinguin2 {
+        constructor(name, alter, geschwindigkeit) {
+            this.name = name;
+            this.alter = alter;
+            this.geschwindigkeit = geschwindigkeit;
+        }
+    }
+
+    /*function Pinguin2(name, alter, geschwindigkeit) 
+    //{
+            this.name = name;
+            this.alter = alter;
+            this.geschwindigkeit = geschwindigkeit;
+    }*/
+
+    var ali = new Pinguin2("ali", 42, 5);
+    var veli = new Pinguin2("Veli", 50, 6);
+
+    for(x in ali)
+    {
+        window.alert(ali[x]);
+    }
+
+}
+
+function Objekt3()
+{
+    class Pinguin3 
+    {
+        constructor(name, alter, geschwindigkeit) 
+        {
+            this.name = name;
+            this.alter = alter;
+            this.geschwindigkeit = geschwindigkeit;
+            this.gehschneller = function(schneller)
+            {
+                this.geschwindigkeit = this.geschwindigkeit + schneller;
+            }
+        }
+    }
+
+    var sait = new Pinguin3("Sait", 50, 7);
+
+    sait.gehschneller(3);
+    window.alert(sait.geschwindigkeit);
+}
+
+function input()
+{
+// Gets user input
+    var name = prompt("What is your name?");
+    var num = prompt("What is your favorite number? ");
+
+    // Uses user input to print out information
+
+    // Prints out the variable type
+    /*
+    document.writeln("Name is a " + typeof name);
+    document.writeln("Num is a " + typeof num);
+    */
+
+    var event3 = document.getElementById("div3");
+    var paragpaph = document.createElement("p");
+
+    paragpaph.innerHTML = "Hello " + name + "!" + "<\p>" + num + "?! That's my favorite number too!";
+
+
+    event3.appendChild(paragpaph);
+}
+
 
 
 
