56bab230dc72a2d07b75b4c23650aa2297e4fd98
Alperen Tunc <115324004+alperen-tunc@users.noreply.github.com>
2023-04-11 09:13:20 +0200

    update_11.03

---
+++ typeScript.ts
@@ -0,0 +1,13 @@
+function fkt(zahl:number):Boolean
+{
+    return true;
+}
+
+var msg:any = null;
+
+// Arrays
+var arr:number[] = [42,3553,65];
+
+var var1:number|string;
+var1 = 42;
+var1 = "String";
