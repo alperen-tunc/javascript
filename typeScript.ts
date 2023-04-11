081c48765b808ab1149402effce6c6008fc3ac40
Alperen Tunc <115324004+alperen-tunc@users.noreply.github.com>
2023-04-11 12:50:17 +0200

    update_11.04

--- typeScript.ts
+++ typeScript.ts
@@ -11,3 +11,14 @@ var arr:number[] = [42,3553,65];
 var var1:number|string;
 var1 = 42;
 var1 = "String";
+
+class Wohnung 
+{
+    zimmer:number;
+    groesse:number;
+    balkon:Boolean;
+}
+
+var meineWohnung:Wohnung = new Wohnung;
+
+meineWohnung.balkon = true;
